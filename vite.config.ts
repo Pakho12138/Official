b44663c68a38a831d3913cf1c6fe4e5892dbfc96
ChenGang <chengang@zzltop.com>
2025-06-27 13:35:56 +0800

    ci：修改配置

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/company/',
+  base: '/',
   plugins: [
     vue(),
     vueJsx(),
